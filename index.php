c53f800758862c87bc598001e379e0922a8ea67f
danielf10 <androdan54@gmail.com>
2016-12-09 13:56:57 -0600

    index
    el inicio

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body ng-controller="ContactosController">
+
+<!--<form action="insertar.php" method="post" name="form" >
+  <input type="text" name="id"><br>
+  <input type="text" name="nombre"><br>
+  <input type="text" name="pw"><br>
+  <input type="submit" value="insertar datos">
+</form>
+-->
+<style type="text/css">
+  #inputnombre{
+    width: 20%;
+
+
+  }
+  body{
+    background-image: url(ag.png);
+    background-repeat: no-repeat;
+    
+  }
+
+ td{
+  margin:5%; 
+ }
+
+ #imagen{
+  background-image: url("call.jpg");
+ }
+</style>
+
+<div class="container">
+
+  
+<img src="call.jpg">
+
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Agenda</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li class="dropdown">
+        <
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          
+        </ul>
+      </li>
+      <li ><a href="agenda.php">Agregar contacto</a></li> 
+      <li><a href="ver.php"> ver y Eliminar contacto</a></li>
+      <li><a href="buscar.php"> buscar</a></li> 
+
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+</table>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+</body>
+</html>
